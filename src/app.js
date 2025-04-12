c914002c36ac29809f9b12933b2168b60096557b
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-04-12 02:56:55 +0000

    correccion a 1ra entrega

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 function getRandomElement(array){
-    return array[Math.floor(Math.random()*4)];
+    return array[Math.floor(Math.random()*array.length)];
 }
 
 

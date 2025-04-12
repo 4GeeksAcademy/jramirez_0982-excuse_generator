597a0690cae680b82e56ad1e69b0ce4900ecdcca
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-04-12 02:21:53 +0000

    entrega Generador Excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function getRandomElement(array){
+    return array[Math.floor(Math.random()*4)];
+}
+
+
 window.onload = function() {
   //write your code here
+
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  
+  let excuse = getRandomElement(who) + " " + getRandomElement(action) + " " + getRandomElement(what) + " " + getRandomElement(when);
+  
+  document.getElementById("excuse").innerText = excuse
   console.log("Hello Rigo from the console!");
 };
